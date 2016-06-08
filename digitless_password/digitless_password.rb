621ff3a9f48f0e1176ff34c2c6221b4f899d9b37
Misha Slyusarev <mik3weider@gmail.com>
2016-06-08 23:58:19 +0300

    Add digitless_password

---
+++ digitless_password/digitless_password.rb
@@ -0,0 +1,17 @@
+def longest_password_length(s)
+  result = -1
+  m = s.scan(/(\D*\p{Uppercase}+\D*)/)
+  m.flatten.map(&:length).max || -1
+end
+
+[
+  'a0Ba',
+  'a0bb',
+  'A',
+  'c',
+  '7aLskw9fD9ad88s',
+  'Laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+].each do |line|
+  puts line
+  puts longest_password_length(line)
+end
